42bdc08ba0992471c836d43a7f42351b3da3714d
Aaro Videvik <videvik@greeny.cs.tlu.ee>
2016-10-06 10:13:18 +0300

    tund4

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+//et saada ligi sessioonile
+	require("functions.php");
+	//kas kasutaja tahab välja logida
+	if(isset($_GET["logout"])){
+		session_destroy();
+		header("Location: login.php");
+		
+	}
+	if(!isset($_SESSION["userId"])){
+			header("Location: login.php");
+		}
+?>
+<h1>Data</h1>
+<p>
+Tere tulemast <?=$_SESSION["userEmail"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p>
